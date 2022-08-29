12116e3d518d343f75f453d9a425b51e85222db3
joe-isaac <isaacjoseph950@gmail.com>
2022-08-29 17:06:22 +0300

    pro Gallery implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import { Menu } from 'antd';
 import './App.css';
 import {Link, Outlet} from 'react-router-dom';
-import 'antd/dist/antd.css'
+import 'antd/dist/antd.css';
+
+
 
 function App() {
+
   return (
-    <div>
-      {/* <img alt='painting background' src='https://www.gn.com/-/media/Images/Page-content-pictures/Legal/Cookie-policy.jpg?h=400&w=2560&la=en&hash=CA125F333E49101BDA01EACB31CB2F78D037EA64'/> */}
+      
     <div>
       <Menu mode='horizontal' style={{marginLeft: '35%', fontFamily: 'arial'}}>
-        <Menu.Item><Link to='./Graffiti'><strong>Graffiti</strong></Link></Menu.Item>
-        <Menu.Item><Link to='./Illustrations'><strong>Illustrations</strong></Link></Menu.Item>
-        <Menu.Item><Link to='./Paintings'><strong>Paintings</strong></Link></Menu.Item>
-        <Menu.Item><Link to='./WaterColors'><strong>WaterColors</strong></Link></Menu.Item>
+        <Menu.Item key={'graffiti'}><Link to='./Graffiti'><strong>Graffiti</strong></Link></Menu.Item>
+        <Menu.Item key={'illustrations'}><Link to='./Illustrations'><strong>Illustrations</strong></Link></Menu.Item>
+        <Menu.Item key={'paintings'}><Link to='./Paintings'><strong>Paintings</strong></Link></Menu.Item>
+        <Menu.Item key={'waterColors'}><Link to='./WaterColors'><strong>WaterColors</strong></Link></Menu.Item>
       </Menu>
-      <div style={{width: '100%', marginLeft: '50%', marginTop:'5%'}}><Outlet/></div>
+      <div style={{marginTop:'5%'}}><Outlet/></div>
     </div>
-      </div>
   );
 }
 
--- src/components/Graffiti.js
+++ src/components/Graffiti.js
@@ -1,8 +1,55 @@
-import React from 'react'
+import {useState, useEffect} from 'react'
+import { ProGallery } from 'pro-gallery';
+import 'pro-gallery/dist/statics/main.css';
 
 const Graffiti = () => {
+const [images, setImages] = useState([]);
+
+useEffect(() => {
+  fetch("http://localhost:8000/images")
+  .then(res => res.json())
+  .then(data => {
+    console.log(data)
+    setImages(data);
+  })
+  .catch(err => alert(err.message))
+}, [])
+
+    
+
+
+// The options of the gallery (from the playground current state)
+const options = {
+      "layoutParams": {
+          "structure": {
+              "galleryLayout": 0
+          }
+      }
+      };
+  
+      // The size of the gallery container. The images will fit themselves in it
+      const container = {
+        width: window.innerWidth,
+        height: window.innerHeight
+      };
+  
+      // The eventsListener will notify you anytime something has happened in the gallery.
+      const eventsListener = (eventName, eventData) => console.log({eventName, eventData}); 
+  
+      // The scrollingElement is usually the window, if you are scrolling inside another element, suplly it here
+      const scrollingElement = window;
   return (
-    <div>Graffiti</div>
+    <div>Graffiti
+      <>
+      {images && <ProGallery
+        items={images}
+        options={options}
+        container={container}
+        eventsListener={eventsListener}
+        scrollingElement={scrollingElement}
+      />}
+      </>
+    </div>
   )
 }
 
